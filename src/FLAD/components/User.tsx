b41c583c9a57ce5f995e7b6cbed63aae62a48233
emkartal1 <emre.kartal@etu.uca.fr>
2023-03-30 14:43:28 +0200

    Create component for messaging :hammer:

---
+++ src/FLAD/components/User.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useSelector } from 'react-redux';
+import { GraphicalCharterDark } from '../assets/GraphicalCharterDark';
+import { GraphicalCharterLight } from '../assets/GraphicalCharterLight';
+import normalize from './Normalize';
+
+type UserProps = {
+    image: string;
+    name: string;
+    lastMessage: string;
+}
+
+
+export default function User() {
+    const isDark = useSelector(state => state.userReducer.dark);
+
+    const style = isDark ? GraphicalCharterDark : GraphicalCharterLight;
+
+    const styles = StyleSheet.create({
+        container: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingVertical: 9,
+        },
+        image: {
+            marginLeft: 15,
+            marginRight: 7,
+            width: 50,
+            height: 50
+        },
+        name: {
+            fontWeight: 'bold',
+            color: style.Text,
+            fontSize: normalize(22)
+        }
+    })
+
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={require("../assets/RedFlady.png")} />
+            <Text style={styles.name} >Emre KARTAL</Text>
+        </View>
+    )
+}
