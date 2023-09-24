bd6d47987c6b04107a1fe6173c04f1ee1e53847a
emkartal1 <emre.kartal@etu.uca.fr>
2023-09-24 15:54:59 +0200

    Delete dist

--- src/Api/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLIENT_SECRET_SPOTIFY = exports.CLIENT_ID_SPOTIFY = exports.MONGO_PASSWORD = void 0;
-exports.MONGO_PASSWORD = "ZXN11dj7VC0RUYwa"; //process.env.MONGO;
-exports.CLIENT_ID_SPOTIFY = "cd55847a89324a9a9af11aa6726bc1f9";
-exports.CLIENT_SECRET_SPOTIFY = "b49e81e3c3a643aa9ce1b2158219a2fb";
-//# sourceMappingURL=config.js.map
