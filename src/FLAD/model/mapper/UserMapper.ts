01de765939d913d3201232e0a4546ef7d73707cc
emkartal1 <emre.kartal@etu.uca.fr>
2023-10-27 23:52:51 +0200

    Delete Model

--- src/FLAD/model/mapper/UserMapper.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../User";
-
-export class UserMapper {
-
-    public static toModel(user: any): User {
-        return new User(user.idFlad, user.idSpotify, user.email, user.createdAt, user.name, user.imageUrl);
-    }
-}
